f927e9e073334cfec68dbc770f03ccf4f68eb3e3
Pedro Moreira <pdrhmoreira@gmail.com>
2024-08-27 14:38:57 +0000

    fix: Added UNIQUE and NOT NULL constraints to elements table

--- schema.sql
+++ schema.sql
@@ -4,3 +4,8 @@ ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
 
 ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
 ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
+
+ALTER TABLE elements ADD CONSTRAINT unique_symbol UNIQUE (symbol);
+ALTER TABLE elements ADD CONSTRAINT unique_name UNIQUE (name);
+ALTER TABLE elements ALTER COLUMN symbol SET NOT NULL;
+ALTER TABLE elements ALTER COLUMN name SET NOT NULL;
