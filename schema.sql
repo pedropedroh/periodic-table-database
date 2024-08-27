18ba751dd0a183e0ac822474d107c3e6b954c578
Pedro Moreira <pdrhmoreira@gmail.com>
2024-08-27 14:38:04 +0000

    fix: Renamed columns in properties table

---
+++ schema.sql
@@ -0,0 +1,3 @@
+ALTER TABLE properties RENAME COLUMN weight TO atomic_mass;
+ALTER TABLE properties RENAME COLUMN melting_point TO melting_point_celsius;
+ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
