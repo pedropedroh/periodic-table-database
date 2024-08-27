b570a44449ccaefab45e30d58a848c2b16f62004
Pedro Moreira <pdrhmoreira@gmail.com>
2024-08-27 14:38:25 +0000

    fix: Added NOT NULL constraints to melting_point_celsius and boiling_point_celsius

--- schema.sql
+++ schema.sql
@@ -1,3 +1,6 @@
 ALTER TABLE properties RENAME COLUMN weight TO atomic_mass;
 ALTER TABLE properties RENAME COLUMN melting_point TO melting_point_celsius;
 ALTER TABLE properties RENAME COLUMN boiling_point TO boiling_point_celsius;
+
+ALTER TABLE properties ALTER COLUMN melting_point_celsius SET NOT NULL;
+ALTER TABLE properties ALTER COLUMN boiling_point_celsius SET NOT NULL;
